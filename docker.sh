b285c332005f399bc5ec24a5bf09aef39be43b7f
Karthikeyan Nallathambi <karthik.aspx.cs@gmail.com>
2023-12-09 23:26:57 -0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-sudo apt update -y
+#sudo apt update -y
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# Add Docker's official GPG key:
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
-sudo apt update -y
+# Install
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-apt-cache policy docker-ce -y
-
-sudo apt install docker-ce -y
-
-#sudo systemctl status docker
-
-sudo chmod 777 /var/run/docker.sock
+sudo docker version
